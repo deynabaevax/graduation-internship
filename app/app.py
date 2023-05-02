ec7ce11000a28ffe5b6b15fefdf130687b335af1
Deyna Baeva <64732465+deynabaevax@users.noreply.github.com>
2023-05-02 14:19:12 +0200

    add the streamlit app

---
+++ app/app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+from PIL import Image
+import pandas as pd
+
+st.set_page_config(page_title = "Main page")
+
+# st.markdown("")
+st.sidebar.markdown("# Main page")
+
+st.title("Automating Topic Modelling")
+# st.write("####  Automating Topic Modelling")
+st.write("##### Created by Deyna Baeva")
+st.write('''Welcome, to the home page! Here, you can select the task you want to perform by choosing from the list on the left.''')
+
+# img = Image.open('ibert.PNG')
+# st.image(img)
+st.markdown('##')
+st.markdown('##')
+st.markdown('##')
+st.markdown('##')
+st.write('### Source Code and Github link')
+st.write('You can access the portfolio which describes the reasoning for building this application on this [link]().')
+st.write('For accessing the source code for this application, you can click on this [link]()')
---
+++ app/pages/bertopic.py
@@ -0,0 +1,9 @@
+import streamlit as st
+from bertopic import BERTopic
+
+st.set_page_config(page_title = "BERTopic")
+
+st.markdown("# Topic Modelling")
+st.subheader("BERTopic")
+
+st.sidebar.markdown("# BERTopic")
